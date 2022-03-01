293d166dbb6f0a7c7f688e9827250616c2cb7e75
AndreiG <ghiranandrei@gmail.com>
2022-03-01 11:30:33 +0200

    added argument support for poplate.c

--- Lab1/populate.c
+++ Lab1/populate.c
@@ -1,24 +1,15 @@
-#include <sys/stat.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <errno.h>
-#include <unistd.h>
+
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <fcntl.h>
 
 int main(int argc, char *argv[])
 {
-
+    int message_number = atoi(argv[1]);
     FILE *fd;
     fd = fopen("messages.txt", "w");
-    for (int i = 1; i < 15000000; i++)
+    for (int i = 0; i < message_number; i++)
     {
         fputs("linie mesaj linie mesaj linie mesaj linie mesaj linie mesaj vlinie mesaj\n", fd);
     }
-    close(fd);
+    fclose(fd);
 }
